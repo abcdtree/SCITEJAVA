db0e26015381d412df326713f5850aa786b6c322
Jianshu Zhang <jianshzhang@yahoo.com>
2017-03-09 11:08:18 -0600

    2 moves finished

--- src/myscite/MySCITE.java
+++ src/myscite/MySCITE.java
@@ -15,6 +15,7 @@ public class MySCITE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*
         // TODO code application logic here
         SciteTree mTree = SciteTree.makeASciteTree("./CsvTrees/VAF-SIMU-NUM-0.csv");
         //System.out.println(mTree.size());
@@ -36,7 +37,15 @@ public class MySCITE {
         double beta = 0.00001;
         
         System.out.println(am.getScore(dm, alpha, beta));
+        */
         
+        //move test
+        int[][] testMatrix = {{1,1,1,1,1,1}, {0,1,0,1,0,0},{0,0,1,0,1,1},{0,0,0,1,0,0},{0,0,0,0,1,0},{0,0,0,0,0,1}};
+        String[] names = {"1","2","3","4","5","6"};
+        AncestorMatrix am = new AncestorMatrix(testMatrix, new MutationNameSpace(names));
+        System.out.println(am);
+        am.swapSubtree(1, 4);
+        System.out.println(am);
     }
     
 }
